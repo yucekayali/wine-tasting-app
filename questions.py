2dae303ece666ac89b33994d4a0a4840c246f716
yucekayali <80151772+yucekayali@users.noreply.github.com>
2025-06-30 22:27:19 +0200

    Create questions.py

---
+++ questions.py
@@ -0,0 +1,16 @@
+appearance_questions = {
+    "color": ["Pale", "Medium", "Deep"],
+    "clarity": ["Clear", "Slightly hazy", "Cloudy"],
+    "legs": ["No legs", "Some legs", "Pronounced legs"]
+}
+
+aroma_questions = {
+    "fruit": ["Citrus", "Red berries", "Tropical", "No fruit"],
+    "other": ["Floral", "Spicy", "Earthy", "Oak", "None"]
+}
+
+taste_questions = {
+    "sweetness": ["Dry", "Off-dry", "Sweet"],
+    "body": ["Light", "Medium", "Full"],
+    "finish": ["Short", "Medium", "Long"]
+}
